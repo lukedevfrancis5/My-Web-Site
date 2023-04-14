c562e8eb39fa509c0c9e43947c1ea67f6d9b0ef8
luke francis <lukedevfrancis@gmail.com>
2023-04-14 18:26:34 -0400

    made Tag class

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 # Create your models here.
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -15,4 +19,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts") # many to one relation
+    tags = models.ManyToManyField(Tag) # many to many relation
