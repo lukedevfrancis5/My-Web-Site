a0abd78df56a83b2a431c38611a8423a79f2d558
luke francis <lukedevfrancis@gmail.com>
2023-04-14 17:34:51 -0400

    created one to many relation in models

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -9,3 +15,4 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
