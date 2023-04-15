0931be00c8da61ebd8f8ee4779cfd7a434128a7d
luke francis <lukedevfrancis@gmail.com>
2023-04-15 06:55:39 -0400

    added null=True to author parameter

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -20,5 +20,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts") #many to one relation
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts") # many to one relation
     tags = models.ManyToManyField(Tag) #many to many relation
