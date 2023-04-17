071e0f243a2b51969106949ee98a8a15b7c52028
luke francis <lukedevfrancis@gmail.com>
2023-04-16 23:46:22 -0400

    added comment

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
-    date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now=True) # gets date
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts") # many to one relation
