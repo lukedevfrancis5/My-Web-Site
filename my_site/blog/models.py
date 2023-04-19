d04b1ac44cb5159856307c4fa6ecf36fe7161a73
luke francis <lukedevfrancis@gmail.com>
2023-04-19 16:22:01 -0400

    added 2 functions

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -6,12 +6,23 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def caption_name(self):
+        return f"{self.caption}"
+    
+    def __str__(self):
+        return self.caption_name()
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email_address = models.EmailField()
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
