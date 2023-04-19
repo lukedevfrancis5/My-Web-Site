49f19c87e4c9e97ef96351249db985ba0157bf9d
luke francis <lukedevfrancis@gmail.com>
2023-04-18 23:39:39 -0400

    added class and fields

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -3,6 +3,12 @@ from .models import Author, Post, Tag
 
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
-admin.site.register(Post)
 admin.site.register(Tag)
